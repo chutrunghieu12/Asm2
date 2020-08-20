8fe5f51a5b84368a4cefca6b21b76c6ea645d2af
chutrunghieu12 <67939197+chutrunghieu12@users.noreply.github.com>
2020-08-20 10:07:42 +0700

    commit on master

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post("/doSearch", async (req, res) => {
   res.render("index1", { model: results });
 });
 app.post("/doDemo", async (req, res) => {
-  //let inputPrice = req.body.txtPrice;
+  let inputPrice = req.body.txtPrice;
   let client = await MongoClient.connect(url);
   let dbo = client.db("ToyDB");
   let results = await dbo
